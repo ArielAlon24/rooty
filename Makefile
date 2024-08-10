5bd120b338fb130f803e6ae1d3288628a1a1a449
ArielAlon24 <arielalon.12321@gmail.com>
2024-08-10 22:50:45 +0000

    Simple kernel module

---
+++ Makefile
@@ -0,0 +1,15 @@
+SRC_DIR = src
+
+obj-m += $(SRC_DIR)/rooty.o
+
+all:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
+clean:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+
+insert:
+	sudo insmod src/rooty.ko
+
+remove:
+	sudo rmmod rooty
--- src/rooty.c
+++ src/rooty.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
-int main(int argc, char const *argv[]) {
-    printf("Hello, Ubuntu!\n");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ariel Alon");
+MODULE_DESCRIPTION("Rooty");
+MODULE_VERSION("0.01");
+
+static int __init rooty_init(void) {
+    printk(KERN_INFO "Hello, world!\n");
     return 0;
 }
+
+static void __exit rooty_exit(void) { printk(KERN_INFO "Goodbye, world!\n"); }
+
+module_init(rooty_init);
+module_exit(rooty_exit);
