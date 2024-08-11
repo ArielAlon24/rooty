653ed8253787c58638dd097b844d94453ffafb89
ArielAlon24 <arielalon.12321@gmail.com>
2024-08-11 13:25:32 +0000

    Add functions hooking with ftrace and hook to the mkdirat command

---
+++ src/ftrace_utils.h
@@ -0,0 +1,142 @@
+#include <linux/ftrace.h>
+#include <linux/kprobes.h>
+#include <linux/linkage.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+#include "logger.h"
+
+#define SYSCALL_NAME(name) "__arm64_sys_" name
+
+#define HOOK(_name, _hook, _orig)                                                                  \
+    {                                                                                              \
+        .name = SYSCALL_NAME(_name),                                                               \
+        .function = (_hook),                                                                       \
+        .original = (_orig),                                                                       \
+    }
+
+#define USE_FENTRY_OFFSET 0
+#if !USE_FENTRY_OFFSET
+#pragma GCC optimize("-fno-optimize-sibling-calls")
+#endif
+
+struct ftrace_hook {
+    const char *name;
+    void *function;
+    void *original;
+
+    unsigned long address;
+    struct ftrace_ops ops;
+};
+
+int fh_install_hook(struct ftrace_hook *hook);
+void fh_remove_hook(struct ftrace_hook *hook);
+int fh_install_hooks(struct ftrace_hook *hooks, size_t count);
+void fh_remove_hooks(struct ftrace_hook *hooks, size_t count);
+
+static int fh_resolve_hook_address(struct ftrace_hook *hook) {
+    struct kprobe kp = {.symbol_name = hook->name};
+
+    int ret = register_kprobe(&kp);
+    if (ret < 0) {
+        WARNING("rooty: register_kprobe failed for %s: %d\n", hook->name, ret);
+        return ret;
+    }
+
+    hook->address = (unsigned long)kp.addr;
+    unregister_kprobe(&kp);
+
+    if (!hook->address) {
+        WARNING("unresolved symbol: %s\n", hook->name);
+        return -ENOENT;
+    }
+
+#if USE_FENTRY_OFFSET
+    *((unsigned long *)hook->original) = hook->address + MCOUNT_INSN_SIZE;
+#else
+    *((unsigned long *)hook->original) = hook->address;
+#endif
+    return 0;
+}
+
+static void fh_ftrace_thunk(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops,
+                            struct ftrace_regs *fregs) {
+
+    struct ftrace_hook *hook = container_of(ops, struct ftrace_hook, ops);
+
+#if USE_FENTRY_OFFSET
+    fregs->pc = (unsigned long)hook->function;
+#else
+    if (!within_module(parent_ip, THIS_MODULE))
+        fregs->pc = (unsigned long)hook->function;
+#endif
+}
+
+int fh_install_hook(struct ftrace_hook *hook) {
+    int err;
+
+    err = fh_resolve_hook_address(hook);
+    if (err)
+        return err;
+
+    hook->ops.func = fh_ftrace_thunk;
+    hook->ops.flags =
+        FTRACE_OPS_FL_SAVE_REGS_IF_SUPPORTED | FTRACE_OPS_FL_IPMODIFY | FTRACE_OPS_FL_RECURSION;
+
+    err = ftrace_set_filter_ip(&hook->ops, hook->address, 0, 0);
+
+    if (err) {
+        WARNING("ftrace_set_filter_ip() failed: %d\n", err);
+        return err;
+    }
+
+    INFO("hook->address = %lx\n", hook->address);
+
+    err = register_ftrace_function(&hook->ops);
+    if (err) {
+        WARNING("register_ftrace_function() failed: %d\n", err);
+        return err;
+    }
+
+    return 0;
+}
+
+void fh_remove_hook(struct ftrace_hook *hook) {
+    int err;
+
+    err = unregister_ftrace_function(&hook->ops);
+    if (err) {
+        WARNING("unregister_ftrace_function() failed: %d\n", err);
+    }
+
+    err = ftrace_set_filter_ip(&hook->ops, hook->address, 1, 0);
+    if (err) {
+        WARNING("ftrace_set_filter_ip() failed: %d\n", err);
+    }
+}
+
+int fh_install_hooks(struct ftrace_hook *hooks, size_t count) {
+    int err;
+    size_t i;
+
+    for (i = 0; i < count; i++) {
+        err = fh_install_hook(&hooks[i]);
+        if (err)
+            goto error;
+    }
+    return 0;
+
+error:
+    while (i != 0) {
+        fh_remove_hook(&hooks[--i]);
+    }
+    return err;
+}
+
+void fh_remove_hooks(struct ftrace_hook *hooks, size_t count) {
+    size_t i;
+
+    for (i = 0; i < count; i++)
+        fh_remove_hook(&hooks[i]);
+}
--- src/rooty.c
+++ src/rooty.c
@@ -2,17 +2,51 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+#include "ftrace_utils.h"
+#include "logger.h"
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ariel Alon");
 MODULE_DESCRIPTION("Rooty");
 MODULE_VERSION("0.01");
 
+static asmlinkage long (*orig_mkdirat)(const struct pt_regs *);
+asmlinkage int hook_mkdirat(const struct pt_regs *regs);
+
+asmlinkage int hook_mkdirat(const struct pt_regs *regs) {
+    // int dfd = regs->regs[0];                       // x0: directory file descriptor
+    char __user *pathname = (char *)regs->regs[1]; // x1: pathname
+    char dir_name[NAME_MAX] = {0};
+
+    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
+
+    if (error > 0) {
+        INFO("Trying to create directory with name: %s\n", dir_name);
+    }
+
+    int result = orig_mkdirat(regs);
+    return result;
+}
+
+static struct ftrace_hook hooks[] = {
+    HOOK("mkdirat", hook_mkdirat, &orig_mkdirat),
+};
+
 static int __init rooty_init(void) {
-    printk(KERN_INFO "Hello, world!\n");
+
+    int err;
+    err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+    if (err)
+        return err;
+
+    INFO("rooty: loaded\n");
     return 0;
 }
 
-static void __exit rooty_exit(void) { printk(KERN_INFO "Goodbye, world!\n"); }
+static void __exit rooty_exit(void) {
+    fh_remove_hooks(hooks, ARRAY_SIZE(hooks));
+    INFO("rooty: unloaded\n");
+}
 
 module_init(rooty_init);
 module_exit(rooty_exit);
